92d7018d2ccafd293d063fa8fe496eaf26c0ca85
elisananda2020-cmd <elisananda2020@gmail.com>
2025-12-31 16:23:12 -0500

    Refactor FIFO algorithm for better readability

--- coodigo-fifo/algoritmoFifo.c
+++ coodigo-fifo/algoritmoFifo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 typedef struct {
     int id;
@@ -12,7 +13,15 @@ typedef struct {
     int response;
 } Processo;
 
+int compararChegada(const void *a, const void *b) {
+    Processo *p1 = (Processo *)a;
+    Processo *p2 = (Processo *)b;
+    return p1->tempoChegada - p2->tempoChegada;
+}
+
 void fifo(Processo p[], int n) {
+    qsort(p, n, sizeof(Processo), compararChegada);
+
     int tempoAtual = 0;
     float somaWT = 0, somaTAT = 0, somaRT = 0;
 
@@ -35,35 +44,25 @@ void fifo(Processo p[], int n) {
 
     printf("\nTabela de Processos (FIFO)\n");
     printf("ID | Chegada | Duracao | Inicio | Termino | WT | TAT | RT\n");
-
     for (int i = 0; i < n; i++) {
         printf("%2d | %7d | %7d | %6d | %7d | %2d | %3d | %2d\n",
-               p[i].id,
-               p[i].tempoChegada,
-               p[i].duracao,
-               p[i].inicio,
-               p[i].termino,
-               p[i].waiting,
-               p[i].turnaround,
-               p[i].response);
+               p[i].id, p[i].tempoChegada, p[i].duracao, p[i].inicio,
+               p[i].termino, p[i].waiting, p[i].turnaround, p[i].response);
     }
 
     printf("\nMetricas do FIFO:\n");
     printf("Media Waiting Time: %.2f\n", somaWT / n);
     printf("Media Turnaround Time: %.2f\n", somaTAT / n);
     printf("Media Response Time: %.2f\n", somaRT / n);
-    printf("Throughput: %.2f processos/unidade de tempo\n",
-           (float)n / tempoAtual);
+    printf("Throughput: %.2f\n", (float)n / tempoAtual);
 }
 
 int main() {
     int n;
-
     printf("Quantidade de processos: ");
-    scanf("%d", &n);
+    if (scanf("%d", &n) != 1 || n <= 0) return 1;
 
     Processo p[n];
-
     for (int i = 0; i < n; i++) {
         p[i].id = i + 1;
         printf("\nProcesso %d\n", p[i].id);
@@ -71,10 +70,8 @@ int main() {
         scanf("%d", &p[i].tempoChegada);
         printf("Duracao: ");
         scanf("%d", &p[i].duracao);
-        p[i].prioridade = 0;
     }
 
     fifo(p, n);
-
     return 0;
 }
